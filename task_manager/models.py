ce84b9d5268cbf9e978072b3b9d47d519c1557a8
Stas Okhrym <s.okhrym@gmail.com>
2022-11-08 21:01:05 +0200

    Adjusted Task fields types and added __str__ methods

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class TaskType(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Position(models.Model):
     name = models.CharField(max_length=65)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Worker(AbstractUser):
@@ -16,6 +22,9 @@ class Worker(AbstractUser):
     class Meta:
         verbose_name = "worker"
         verbose_name_plural = "workers"
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
 
 class Task(models.Model):
@@ -29,11 +38,14 @@ class Task(models.Model):
     description = models.TextField()
     deadline = models.DateField()
     is_completed = models.BooleanField()
-    priority = models.CharField(
-        max_length=20, choices=PRIORITY_CHOICES, default="medium"
+    priority = models.IntegerField(
+        choices=PRIORITY_CHOICES, default="medium"
     )
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     assignees = models.ManyToManyField(Worker, related_name="tasks")
 
     class Meta:
         ordering = ["priority"]
+
+    def __str__(self) -> str:
+        return f"{self.name} (Priority: {self.priority})"
