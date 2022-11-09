c508802283a7dbdcfe1c268668bdc02d3c3cd2d1
Stas Okhrym <s.okhrym@gmail.com>
2022-11-09 12:41:15 +0200

    Added base.html and index view and .html

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.utils.datetime_safe import datetime
 
-# Create your views here.
+from task_manager.models import Worker, Task
+
+
+def index(request):
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+    closest_deadline = min([(task.deadline - datetime.now().date()).days for task in Task.objects.all()])
+
+    num_visits = request.session.get("num_visits", 1)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+        "closest_deadline": closest_deadline,
+        "num_visits": num_visits,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
