ed48f4c58d580b54892501d03f74a64d9a337a1b
Stas Okhrym <s.okhrym@gmail.com>
2022-11-10 16:27:16 +0200

    Added urls for task_type

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -7,6 +7,11 @@ from .views import (
     TaskCreateView,
     TaskUpdateView,
     TaskDeleteView,
+    TaskTypeListView,
+    TaskTypeDetailView,
+    TaskTypeCreateView,
+    TaskTypeUpdateView,
+    TaskTypeDeleteView,
     WorkerListView,
     WorkerDetailView,
     WorkerCreateView,
@@ -23,6 +28,11 @@ from .views import (
 
 urlpatterns = [
     path("", index, name="index"),
+    path("task_types/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("task_types/<int:pk>/", TaskTypeDetailView.as_view(), name="task-type-detail"),
+    path("task_types/create/", TaskTypeCreateView.as_view(), name="task-create"),
+    path("task_types/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-update"),
+    path("task_types/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-delete"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
